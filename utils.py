4b6535b1b7aa2f736ddd0a575bdc57a85ec71c3a
Purevsuren Erdene <purevsuren.er@gmail.com>
2023-11-02 20:55:52 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import re
 
 # Constants
-EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]{6,}+@university\.com$"
+EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@university\.com$"
 PASSWORD_REGEX = r"^(?=.*[A-Z])(?=.*\d).{6,}$"
 
 
